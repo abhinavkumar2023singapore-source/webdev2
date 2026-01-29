c6914758edb8cd8733f7a9f1fb7ddfa60ba0f698
user.name "Master Abhinav <user.email "mailabhinavbunty7577000@gmail.com>
2026-01-29 15:49:21 +0530

    Initial commit

--- javascript/script.js
+++ javascript/script.js
@@ -12,7 +12,7 @@ function print (){
 
 // console.log ("this is  the begining of the code")
 
-let value = 0;
+/*let value = 0;
 
 let interval = setInterval(() => {
     console.log(value);
@@ -22,3 +22,42 @@ let interval = setInterval(() => {
         clearInterval(interval);
     }
 }, 1000);
+*/
+/*
+const studentName = document.querySelector('#name')
+const button = document.querySelector('.btn')
+const list = document.querySelector('.list')
+
+button.addEventListener('click', () => {
+    //creating the  element
+    const li = document.createElement('li')
+    const deleteButton = document.createElement('button')
+        //providding the text 
+
+    li.innerText = studentName.value
+    deleteButton.innerText = "Delete"
+
+
+    deleteButton.addEventListener('click', () => {
+        // li.remove()
+        list.removeChild(li)
+    })
+    li.appendChild(deleteButton)
+    list.appendChild(li)
+    studentName.value = ""
+})
+*/
+function print() {
+    console.log("inside print ")
+}
+
+function greet(print) { //hgher oerder function
+    setTimeout(() => {
+
+
+
+        console.log("hello students")
+        print()
+    }, 2000);
+}
+greet(print)
