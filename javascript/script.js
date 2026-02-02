c8cf45335d14c9f7b572062fd1b67bd2288e1ddb
user.name "Master Abhinav <user.email "mailabhinavbunty7577000@gmail.com>
2026-02-02 12:06:45 +0530

    function

--- javascript/script.js
+++ javascript/script.js
@@ -47,7 +47,7 @@ button.addEventListener('click', () => {
     studentName.value = ""
 })
 */
-function print() {
+/*function print() {
     console.log("inside print ")
 }
 
@@ -61,3 +61,43 @@ function greet(print) { //hgher oerder function
     }, 2000);
 }
 greet(print)
+*/
+/*console.log("Starting homework...");
+setTimeout(() => {
+
+    console.log("Homework done!");
+    console.log("Starting dinner...");
+    setTimeout(() => {
+        console.log("Dinner done!");
+        console.log("Getting ready to go out...");
+        setTimeout(() => {
+            console.log("Going to the playground!");
+        }, 1000); // after dinner
+    }, 1500); // dinner time
+}, 2000); // homework time
+*/
+
+function finishHomework(callback) {
+    console.log("Starting homework...");
+    setTimeout(() => {
+        console.log("Homework done!");
+        callback();
+    }, 2000);
+}
+
+function eatDinner(callback) {
+    console.log("Starting dinner...");
+    setTimeout(() => {
+        console.log("Dinner done!");
+        callback();
+    }, 1500);
+}
+
+function goToPlayground() {}
+console.log("Going to the playground!");
+// Chained in steps, but cleaner 
+finishHomework(() => {
+    eatDinner(() => {
+        goToPlayground();
+    });
+});
